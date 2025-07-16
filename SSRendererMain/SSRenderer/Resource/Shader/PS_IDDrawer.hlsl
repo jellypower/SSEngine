7a15f4afe7761b5c940f51fb711c20774e1d72e1
jellypower <happy981024@naver.com>
2025-07-16 18:34:39 +0900

    이전 에디터 솔루션 삭제

--- SSRendererMain/SSRenderer/Resource/Shader/PS_IDDrawer.hlsl
+++
@@ -1,7 +0,0 @@
-#include "include/CBSet/IS_DefaultPbrCBSet.hlsl"
-
-int2 Main(PS_INPUT_DEFAULT input) : SV_Target
-{
-    int2 Result = int2(Id.LSB, Id.MSB);
-    return Result;
-}
--- SSRendererMain/SSRenderer/Resource/Shader/include/CBSet/IS_SKMDefaultPBRCBSet.hlsl
+++
@@ -1,6 +0,0 @@
-// StaticMeshToDefaultPSInput
-#include "include/Types/IS_DefaultTypes.hlsl"
-
-
-StructuredBuffer<Joint> SkeletonJointInverse : register(t5);
-StructuredBuffer<Joint> CurrentJoint : register(t6);
