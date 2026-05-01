e7f5586a1ebfd9d83547deffa2123ccbb667f268
jellypower <happy981024@naver.com>
2026-05-01 18:20:27 +0900

    imgui 서브모듈으로 추가

---
+++ _BuildBatch/Build_Dependancy_x64.bat
@@ -0,0 +1,121 @@
+@echo off
+setlocal EnableDelayedExpansion
+
+
+:: ======================================================= Setup ======================================================= 
+
+echo ================ Build_Dependancy_Debug_x64 ================
+
+set MSBUILD_PATH="C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe"
+
+
+set SSGAL_MODULE_NAME=SSGAL
+set SSGAL_MODULE_PATH="..\!SSGAL_MODULE_NAME!\!SSGAL_MODULE_NAME!.sln"
+
+
+set "PHYSX_BUILD_OPTION_DEBUG=/m /nologo /verbosity:quiet /clp:ErrorsOnly;NoSummary /p:Configuration="Debug""
+set "PHYSX_BUILD_OPTION_CHECKED=/m /nologo /verbosity:quiet /clp:ErrorsOnly;NoSummary /p:Configuration="checked""
+
+set PHYSX_BULID_TOOL_PATH="..\PhysX\physx"
+set PHYSX_BUILD_SLN_PATH="..\PhysX\physx\compiler\vc17win64-cpu-only\PhysXSDK.sln"
+
+set PHYSX_BUILD_RESULT_PATH_DEBUG=..\PhysX\physx\bin\win.x86_64.vc143.md\debug
+set PHYSX_BUILD_RESULT_PATH_CHECKED=..\PhysX\physx\bin\win.x86_64.vc143.md\checked
+
+
+set PHYSX_NEEDED_MODULE_NAMES[0]=LowLevel_static_64
+set PHYSX_NEEDED_MODULE_NAMES[1]=LowLevelAABB_static_64
+set PHYSX_NEEDED_MODULE_NAMES[2]=LowLevelDynamics_static_64
+set PHYSX_NEEDED_MODULE_NAMES[3]=PhysX_64
+set PHYSX_NEEDED_MODULE_NAMES[4]=PhysXCharacterKinematic_static_64
+set PHYSX_NEEDED_MODULE_NAMES[5]=PhysXCommon_64
+set PHYSX_NEEDED_MODULE_NAMES[6]=PhysXCooking_64
+set PHYSX_NEEDED_MODULE_NAMES[7]=PhysXExtensions_static_64
+set PHYSX_NEEDED_MODULE_NAMES[8]=PhysXFoundation_64
+set PHYSX_NEEDED_MODULE_NAMES[9]=PhysXPvdSDK_static_64
+set PHYSX_NEEDED_MODULE_NAMES[10]=PhysXTask_static_64
+set PHYSX_NEEDED_MODULE_NAMES[11]=PVDRuntime_64
+set PHYSX_NEEDED_MODULE_NAMES[12]=SceneQuery_static_64
+set PHYSX_NEEDED_MODULE_NAMES[13]=SimulationController_static_64
+set PHYSX_NEEDED_MODULE_CNT=13
+
+for /L %%i in (0,1,%PHYSX_NEEDED_MODULE_CNT%) do (
+    set PHYSX_MODULE_PREFIX_DEBUG[%%i]=!PHYSX_BUILD_RESULT_PATH_DEBUG!\!PHYSX_NEEDED_MODULE_NAMES[%%i]!
+    set PHYSX_MODULE_PREFIX_CHECKED[%%i]=!PHYSX_BUILD_RESULT_PATH_CHECKED!\!PHYSX_NEEDED_MODULE_NAMES[%%i]!
+)
+
+
+set SSENGINE_PHYSX_BUILD_RESULT_DEBUG=..\_BuildResults\PhysX_Debug_x64
+set SSENGINE_PHYSX_BUILD_RESULT_RELEASE=..\_BuildResults\PhysX_Checked_x64
+
+
+
+:: ======================================================= Run ======================================================= 
+
+
+echo.
+echo.
+echo     ================ Restore_DX12_Nuget_Package ================
+echo.
+%MSBUILD_PATH% %SSGAL_MODULE_PATH% /t:Restore
+echo     ================ ~Restore_DX12_Nuget_Package ================
+
+
+echo.
+echo.
+echo     ================ PhysX_Git_Submodule_Update ================
+echo.
+git submodule update --init --recursive
+echo     ================ ~PhysX_Git_Submodule_Update ================
+
+
+echo.
+echo.
+echo     ================ PhysX_Generate_Project ================
+echo.
+call %PHYSX_BULID_TOOL_PATH%/generate_projects.bat vc17win64-cpu-only
+echo     ================ ~PhysX_Generate_Project ================
+
+
+echo.
+echo.
+echo     ================ PhysX_Run_Build ================
+echo.
+echo     Building with Debug option... 
+%MSBUILD_PATH% %PHYSX_BUILD_SLN_PATH% %PHYSX_BUILD_OPTION_DEBUG%
+echo     Building with Checked option... 
+%MSBUILD_PATH% %PHYSX_BUILD_SLN_PATH% %PHYSX_BUILD_OPTION_CHECKED%
+echo     ================ ~PhysX_Run_Build ================
+
+
+echo.
+echo.
+echo     ================ PhysX_Copy_BuildResult ================
+echo.
+
+if %ERRORLEVEL% neq 0 (
+     echo         @@@@@@@ PHYSX BUILD FAILED !!! Errno: %ERRORLEVEL% @@@@@@@
+)
+
+if not exist "../_BuildResults/PhysX_Debug_x64" mkdir "../_BuildResults/PhysX_Debug_x64"
+for /L %%i in (0,1,%PHYSX_NEEDED_MODULE_CNT%) do (
+
+    copy /y "!PHYSX_MODULE_PREFIX_DEBUG[%%i]!.*" "%SSENGINE_PHYSX_BUILD_RESULT_DEBUG%\"
+)
+
+if not exist "../_BuildResults/PhysX_Checked_x64" mkdir "../_BuildResults/PhysX_Checked_x64"
+for /L %%i in (0,1,%PHYSX_NEEDED_MODULE_CNT%) do (
+
+    copy /y "!PHYSX_MODULE_PREFIX_DEBUG[%%i]!.*" "%SSENGINE_PHYSX_BUILD_RESULT_RELEASE%/"
+)
+
+
+echo     ================ ~PhysX_Copy_BuildResult ================
+
+
+echo ================ ~Build_Dependancy_Debug_x64 ================
+echo.
+
+pause
+
+endlocal
